473cbe5b837a0781df971d643baa90f7314f4963
GIT ICT Spark Holland <git.ict@sparkholland.com>
2024-12-05 15:48:45 +0100

    demo 1 ready

--- packages/volto-spark-theme/src/index.js
+++ packages/volto-spark-theme/src/index.js
@@ -1,3 +1,9 @@
+import Edit from '../node_modules/@kitconcept/volto-slider-block/src/components/Edit';
+import View from './components/volto-slider-block/View';
+import sliderSVG from '@plone/volto/icons/slider.svg';
+import { SliderBlockDataAdapter } from '../node_modules/@kitconcept/volto-slider-block/src/components/adapter';
+
+import '../node_modules/@kitconcept/volto-slider-block/src/theme/main.less';
 const applyConfig = (config) => {
   config.blocks.themes = [
     {
@@ -20,7 +26,30 @@ const applyConfig = (config) => {
       name: 'green',
       label: 'Green',
     },
+    {
+      style: {
+        '--theme-color': '#293676',
+        '--theme-high-contrast-color': '#5e6a72',
+        '--theme-foreground-color': 'white',
+        '--theme-low-contrast-foreground-color': 'lightgrey',
+      },
+      name: 'sparkblue',
+      label: 'SparkBlue',
+    },
   ];
+  config.blocks.blocksConfig.slider = {
+    id: 'slider',
+    title: 'Slider',
+    group: 'common',
+    icon: sliderSVG,
+    view: View,
+    edit: Edit,
+    restricted: false,
+    mostUsed: true,
+    sidebarTab: 1,
+    dataAdapter: SliderBlockDataAdapter,
+    enableAutoPlay: false,
+  };
   return config;
 };
 
